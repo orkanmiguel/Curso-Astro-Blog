f4fc483b9c55627a3bda51e6d7063771031bff97
orkanmiguel <orkan.miguel@gmail.com>
2024-10-15 00:16:42 -0300

    transition and quality

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,18 +2,21 @@ import { defineCollection, z } from "astro:content";
 
 const blogCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    date: z.date(),
-    description: z.string(),
-    image: z.string(),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.date(),
+      description: z.string(),
+      image: image().refine((imag) => imag.width < 1200, {
+        message: "Image should be lower than 50000px",
+      }),
 
-    //Relacion
-    author: z.string(),
+      //Relacion
+      author: z.string(),
 
-    //relacion
-    tags: z.array(z.string()),
-  }),
+      //relacion
+      tags: z.array(z.string()),
+    }),
 });
 
 export const collections = {
